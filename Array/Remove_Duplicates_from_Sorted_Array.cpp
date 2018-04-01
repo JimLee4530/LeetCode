ef1387ca3104281ec73dd2643641742772e86ebe
j1ml3e <542243844@qq.com>
2018-04-01 23:19:57 +0800

    class Solution {

---
+++ Array/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        int count = 1;
+        int i = nums[0];
+        for(int num : nums){
+            if(num != i){
+                nums[count] = num;
+                i=num;
+                count++;
+            }
+        }
+        return count;
+    }
+};
